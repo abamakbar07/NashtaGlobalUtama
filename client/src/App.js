4fd5aa7d27b029ba862e7f09cbcd7a28ab8559c9
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-24 23:55:44 +0700

    add eventForm

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './pages/components/Navbar'
+import FormAddEvent from './pages/form/FormAddEvent';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <FormAddEvent />
     </div>
   );
 }
---
+++ client/src/pages/form/FormAddEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormAddEvent = () => {
+   return (
+     <div className="FormAddEvent p-5">
+        Home
+     </div>
+   );
+}
+
+export default FormAddEvent
