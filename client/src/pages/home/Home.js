ca5319cb1673834eff0343a4e902c0e29b0c3672
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-25 01:30:18 +0700

    add home

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import { Card, Row, Button } from 'react-bootstrap'
 
 const Home = () => {
+   const data = [
+      {
+         title: "Task pertama",
+         alamat: "Disini"
+      },
+      {
+         title: "Task kedua",
+         alamat: "Disana"
+      },
+      {
+         title: "Task ketiga",
+         alamat: "Disitu"
+      },
+   ]
+
    return (
-      <div className="Home">
-         Home
+      <div className="Home bg-danger">
+         <div className="pt-5 d-flex" style={{overflowX: "scroll"}}>
+            {data.map(content => (
+               <div className="CardContainer">
+                  <Card style={{ width: "240px" }}>
+                     <Card.Img variant="top" src="holder.js/100px180" />
+                     <Card.Body>
+                        <Card.Title>{content.title}</Card.Title>
+                        <Card.Text>
+                           {content.alamat}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                     </Card.Body>
+                  </Card>
+               </div>
+            ))}
+         </div>
       </div>
-   )
+   );
 }
 
 export default Home
