ba3c7354a3a11bf7fd027ebc6e451cddad7591a5
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-25 11:30:13 +0700

    restyle home card

--- client/src/pages/home/CardContainer.js
+++ client/src/pages/home/CardContainer.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { Card, Button } from 'react-bootstrap';
 
 const CardContainer = (props) => {
+   const data = props.data
+
    return (
       <div className="CardContainer">
          <Card style={{ width: "240px" }}>
             <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
-               <Card.Title>{props.title}</Card.Title>
-               <Card.Text>{props.address}</Card.Text>
+               <div className="title Location">{data.location}</div>
+               <div className="title Title">{data.title}</div>
+               <div className="title Date">{data.date}</div>
+               <div className="desc Participant">{data.participant}</div>
+               <div className="desc Note">{data.note}</div>
                <Button variant="primary">Go somewhere</Button>
             </Card.Body>
          </Card>
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,27 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { API } from '../../config/api';
 import CardContainer from './CardContainer';
 
 const Home = () => {
-   const data = [
-      {
-         title: "Task pertama",
-         address: "Disini"
-      },
-      {
-         title: "Task kedua",
-         address: "Disana"
-      },
-      {
-         title: "Task ketiga",
-         address: "Disitu"
-      },
-   ]
+   const [data, setData] = useState([])
+
+   useEffect(() => {
+      API.get(`/tasks`).then((res) => {
+         setData(
+            res.data
+         );
+      });
+   }, []);
 
    return (
       <div className="Home bg-danger">
          <div className="pt-5 d-flex" style={{overflowX: "scroll"}}>
             {data.map(content => (
-               <CardContainer title={content.title} address={content.address} />
+               <CardContainer data={content} />
             ))}
          </div>
       </div>
