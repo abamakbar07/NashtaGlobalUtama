a88765511f2146e72e3fe4ed6abf83ddd5bd7ca5
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-25 13:18:49 +0700

    get picture from server

--- client/src/pages/home/CardContainer.js
+++ client/src/pages/home/CardContainer.js
@@ -6,25 +6,23 @@ const CardContainer = (props) => {
    const participants = data.participant.split(",")
 
    return (
-      <div className="CardContainer">
-         <Card style={{ width: "240px" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-               <div className="title Location">{data.location}</div>
-               <div className="title Title">{data.title}</div>
-               <div className="title Date">{data.date}</div>
-               <div className="desc Participant d-flex">
-                  {participants.map((participant) => (
-                     <div className="bg-dark text-white m-1 p-1">
-                        {participant}
-                     </div>
-                  ))}
-               </div>
-               <div className="desc Note">{data.note}</div>
-               <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-         </Card>
-      </div>
+     <div className="CardContainer">
+       <Card style={{ width: "240px" }}>
+         <Card.Img variant="top" src={`http://localhost:5000/tasks/${data.picture}`} />
+         <Card.Body>
+           <div className="title Location">{data.location}</div>
+           <div className="title Title">{data.title}</div>
+           <div className="title Date">{data.date}</div>
+           <div className="desc Participant d-flex">
+             {participants.map((participant) => (
+               <div className="bg-dark text-white m-1 p-1">{participant}</div>
+             ))}
+           </div>
+           <div className="desc Note">{data.note}</div>
+           <Button variant="primary">Go somewhere</Button>
+         </Card.Body>
+       </Card>
+     </div>
    );
 }
 
